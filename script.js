bf4d0c9b33db118616503ca900284f1b2963f169
Utkarsh Kumar Patel <88320546+Utkarsh7545@users.noreply.github.com>
2023-03-29 20:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function hello() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		},1000);
+	});
+}
+
+hello().then((data) => {
+	data=document.getElementById("output").innerHTML;
+});
